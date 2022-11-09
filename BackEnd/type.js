46a2b09ec33580bb836142dc50033c0ceb3b3da3
Pedro Matos <pedro_matos2012@hotmail.com>
2022-11-09 00:14:17 -0400

    Create type.js

---
+++ BackEnd/type.js
@@ -0,0 +1,28 @@
+// Tipo Number
+
+const primeiroNumero = 1.5; // ponto flutuante ou float
+const segundoNumero = 2; // inteiro
+const operacaoMatematica = primeiroNumero - segundoNumero; // operação matemática entre variáveis
+
+const alura = "Alura"; // NaN -> Not A Number
+//console.log(alura / primeiroNumero);
+
+
+// Tipo String
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+// Concatenação (+)
+const citacao = "Meu nome é "
+const meuNome = "Pedro";
+//console.log(citacao + meuNome);
+
+// Tipo Boolean
+const numeroUm = 5;
+const numeroDois = 10;
+const numeroTres = 5;
+/*console.log(numeroUm === numeroDois); //5 = 10 é falso
+console.log(numeroUm === numeroTres); //5 = 5 é verdadeiro*/
+
